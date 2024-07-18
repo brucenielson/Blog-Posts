14196562494a16e70ae9394d47b7ef84a2c7066b
Bruce Nielson <bnielson@mindfiretechnology.com>
2024-07-18 09:22:24 -0600

    Added commented out cuda stuff.

--- gemma_demo.py
+++ gemma_demo.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
 from huggingface_hub import login
+# import torch
 
 # Use huggingface-cli login
 secret_file = r'D:\Documents\Secrets\huggingface_secret.txt'
@@ -17,10 +18,13 @@ model_name = 'google/gemma-1.1-2b-it'
 tokenizer = AutoTokenizer.from_pretrained(model_name)  # google/gemma-2-9b-it
 model = AutoModelForCausalLM.from_pretrained(
     model_name
+    # ,
+    # device_map="auto",
+    # torch_dtype=torch.bfloat16
 )
 
 input_text = "Write me a poem with rhyming lines about a Dungeons and Dragons adventure."
-inputs = tokenizer(input_text, return_tensors="pt")
+inputs = tokenizer(input_text, return_tensors="pt")#.to("cuda")
 # https://huggingface.co/docs/transformers/v4.42.0/en/internal/generation_utils#transformers.TextStreamer
 # https://huggingface.co/docs/text-generation-inference/conceptual/streaming
 # https://www.gradio.app/guides/quickstart
